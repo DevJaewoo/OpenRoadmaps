7e810a8194690abdee0996eada2ac1043dafb468
Jaewoo Ahn <DevJaewoo@gmail.com>
2022-09-27 22:29:17 +0900

    Initial Commit

---
+++ backend/src/main/java/com/devjaewoo/openroadmaps/OpenRoadmapsApplication.java
@@ -0,0 +1,13 @@
+package com.devjaewoo.openroadmaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenRoadmapsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenRoadmapsApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/devjaewoo/openroadmaps/OpenRoadmapsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjaewoo.openroadmaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenRoadmapsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
