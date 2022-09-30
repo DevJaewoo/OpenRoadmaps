de252f08478ab4d02b9d089b0cc764c269e185e9
Jaewoo Ahn <DevJaewoo@gmail.com>
2022-09-30 09:39:06 +0900

    Move Navigation to components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navigation from "./routes/Navigation";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
