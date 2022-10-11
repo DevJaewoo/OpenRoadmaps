b7d83aac0d754febb2cd3e275275a0c515478a07
Jaewoo Ahn <DevJaewoo@gmail.com>
2022-10-11 21:25:46 +0900

    feat: OAuth2 회원가입 컴포넌트 개발

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navigation from "./components/Navigation";
 import NotFound from "./pages/error/NotFound";
+import Register from "./pages/login/register";
 import Main from "./pages/main/Main";
 
 function App() {
   return (
-    <div className="App min-w-[640px] pt-16">
+    <div className="flex flex-col App min-w-[640px] min-h-screen pt-16">
       <BrowserRouter>
         <Navigation />
         <Routes>
           <Route path="/" element={<Main />} />
+          <Route path="/register" element={<Register />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
