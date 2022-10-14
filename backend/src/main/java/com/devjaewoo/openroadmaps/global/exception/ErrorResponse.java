de785c82c085d850d8bd570b8b7c695bf8aad1da
Jaewoo Ahn <DevJaewoo@gmail.com>
2022-10-14 22:43:55 +0900

    Feat(backend): Validation Exception Handler 추가

--- backend/src/main/java/com/devjaewoo/openroadmaps/global/exception/ErrorResponse.java
+++ backend/src/main/java/com/devjaewoo/openroadmaps/global/exception/ErrorResponse.java
@@ -1,7 +1,18 @@
 package com.devjaewoo.openroadmaps.global.exception;
 
-public record ErrorResponse(String code, String message) {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import org.springframework.validation.FieldError;
+
+import java.util.List;
+
+public record ErrorResponse(String code, String message, @JsonInclude(JsonInclude.Include.NON_EMPTY) List<ValidationError> errors) {
     public ErrorResponse(ErrorCode errorCode) {
-        this(errorCode.name(), errorCode.getMessage());
+        this(errorCode.name(), errorCode.getMessage(), null);
+    }
+
+    public record ValidationError(String field, String message) {
+        public ValidationError(FieldError fieldError) {
+            this(fieldError.getField(), fieldError.getDefaultMessage());
+        }
     }
 }
