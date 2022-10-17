3ecb4b5497be3ced9d3628ccc34840f54b6d52b0
Jaewoo Ahn <DevJaewoo@gmail.com>
2022-10-17 17:53:29 +0900

    Feat(frontend): Login form 및 제약조건 개발

--- frontend/src/pages/login/register.tsx
+++ frontend/src/pages/login/register.tsx
@@ -2,19 +2,28 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { PrimaryButton } from "../../components/button/VariantButtons";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
+import { useRef } from "react";
 
 const Register: React.FC<{}> = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const password = useRef({});
+  password.current = watch("password", "");
 
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    console.log(data);
+    const callback = async () => {
+      await setTimeout(() => console.log(data), 1000);
+    };
+    callback();
   };
 
   return (
-    <div className="flex flex-col items-center flex-1 w-full h-full">
-      <h1 className="w-full mt-10 text-center text-3xl font-semibold">
-        회원가입
-      </h1>
+    <div className="flex flex-col items-center flex-1 w-full h-full my-10">
+      <h1 className="w-full text-center text-3xl font-semibold">회원가입</h1>
       <form
         onSubmit={handleSubmit(onSubmit)}
         className="flex flex-col w-full max-w-2xl mt-5 p-10 border-gray-100 border rounded-xl shadow-md"
@@ -25,8 +34,17 @@ const Register: React.FC<{}> = () => {
             type="email"
             placeholder="example@email.com"
             className="w-full mt-2 p-3 border border-gray-300 rounded-md"
-            {...register("email")}
+            {...register("email", {
+              required: "이메일을 입력해주세요.",
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "이메일 형식이 올바르지 않습니다.",
+              },
+            })}
           />
+          {errors.email && (
+            <small role="alert">{errors.email.message?.toString()}</small>
+          )}
         </div>
         <div className="w-full mb-5">
           <h3 className="text-lg">비밀번호</h3>
@@ -34,8 +52,19 @@ const Register: React.FC<{}> = () => {
             type="password"
             placeholder="Enter password"
             className="w-full mt-2 p-3 border border-gray-300 rounded-md"
-            {...register("password")}
+            {...register("password", {
+              required: "비밀번호를 입력해주세요.",
+              pattern: {
+                value:
+                  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,14}$/,
+                message:
+                  "하나 이상의 대문자, 소문자, 숫자, 특수문자를 포함한 8~14 자리의 비밀번호이어야 합니다.",
+              },
+            })}
           />
+          {errors.password && (
+            <small role="alert">{errors.password.message?.toString()}</small>
+          )}
         </div>
         <div className="w-full mb-5">
           <h3 className="text-lg">비밀번호 확인</h3>
@@ -43,8 +72,14 @@ const Register: React.FC<{}> = () => {
             type="password"
             placeholder="Re-enter password"
             className="w-full mt-2 p-3 border border-gray-300 rounded-md"
-            {...register("repassword")}
+            {...register("repassword", {
+              validate: (value) =>
+                value === password.current || "비밀번호가 일치하지 않습니다.",
+            })}
           />
+          {errors.repassword && (
+            <small role="alert">{errors.repassword.message?.toString()}</small>
+          )}
         </div>
 
         <PrimaryButton
@@ -52,9 +87,6 @@ const Register: React.FC<{}> = () => {
           text="회원가입"
           to="http://localhost:8080/oauth2/authorization/google"
           class="w-full mt-4 py-3 justify-center text-xl"
-          onClick={() => {
-            console.log("Click!");
-          }}
         />
       </form>
 
