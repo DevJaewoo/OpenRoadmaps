f21d5fa46ecba82018f545fbc302a2dfb5b23ca5
Jaewoo Ahn <DevJaewoo@gmail.com>
2022-10-26 20:25:38 +0900

    Feat(backend): JPAQueryFactory Bean 추가

--- backend/src/main/java/com/devjaewoo/openroadmaps/global/config/SpringConfig.java
+++ backend/src/main/java/com/devjaewoo/openroadmaps/global/config/SpringConfig.java
@@ -1,9 +1,12 @@
 package com.devjaewoo.openroadmaps.global.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.persistence.EntityManager;
 
 @Configuration
 public class SpringConfig {
@@ -11,5 +14,10 @@ public class SpringConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager em) {
+        return new JPAQueryFactory(em);
     }
 }
