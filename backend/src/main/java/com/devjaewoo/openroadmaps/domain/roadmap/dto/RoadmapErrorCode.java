283efad0ab787a1e0a2c7bede2707a7764c26e0e
Jaewoo Ahn <DevJaewoo@gmail.com>
2022-10-30 16:04:13 +0900

    Refactor(backend): ErrorCode Field public으로 변경

--- backend/src/main/java/com/devjaewoo/openroadmaps/domain/roadmap/dto/RoadmapErrorCode.java
+++ backend/src/main/java/com/devjaewoo/openroadmaps/domain/roadmap/dto/RoadmapErrorCode.java
@@ -8,8 +8,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum RoadmapErrorCode implements ErrorCode {
-    INVALID_PARENT(HttpStatus.BAD_REQUEST, "존재하지 않는 부모 노드를 참조하고 있습니다.");
+    INVALID_PARENT(HttpStatus.BAD_REQUEST, "존재하지 않는 부모 노드를 참조하고 있습니다."),
+    INVALID_CONNECTION(HttpStatus.BAD_REQUEST, "부모 노드와 연결되어있어야 합니다.");
 
-    private final HttpStatus httpStatus;
-    private final String message;
+    public final HttpStatus httpStatus;
+    public final String message;
 }
