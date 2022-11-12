7a06ea5439c3f61789ae3b217544959ce0ab01ef
DevJaewoo <DevJaewoo@gmail.com>
2022-11-12 11:31:56 +0900

    Feat(frontend): Drag 불가능한 컴포넌트 cursor default로 설정

--- frontend/src/pages/roadmaps/roadmapEdit/_RoadmapEditItem.tsx
+++ frontend/src/pages/roadmaps/roadmapEdit/_RoadmapEditItem.tsx
@@ -61,7 +61,7 @@ const RoadmapEditItem: FC<Props> = ({
         type="button"
         className={`flex justify-center items-center absolute max-w-xs px-5 py-2 bg-white border-4 rounded-xl border-${
           RoadmapColor[roadmapItem.recommend]
-        }`}
+        } ${disabled ? "cursor-default" : "cursor-pointer"}`}
         style={{ top: `${defaultCoord.y}rem`, left: `${defaultCoord.x}rem` }}
         onClick={handleClick}
         onDoubleClick={handleDoubleClick}
