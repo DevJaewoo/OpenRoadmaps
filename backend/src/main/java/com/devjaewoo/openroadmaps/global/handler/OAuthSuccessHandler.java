98aaae454fe57e9e487294a89f3ef6a6c7283ce8
DevJaewoo <DevJaewoo@gmail.com>
2022-12-03 10:48:19 +0900

    Fix(backend): OAuth2 Redirect URI application.yml에 등록

--- backend/src/main/java/com/devjaewoo/openroadmaps/global/handler/OAuthSuccessHandler.java
+++ backend/src/main/java/com/devjaewoo/openroadmaps/global/handler/OAuthSuccessHandler.java
@@ -1,5 +1,6 @@
 package com.devjaewoo.openroadmaps.global.handler;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,9 @@ import java.io.IOException;
 @Component
 public class OAuthSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
+    @Value("${spring.oauth2.success-uri:http://localhost:3000}")
+    private String successUrl;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         String targetUri = determineTargetUrl(request, response, authentication);
@@ -20,6 +24,6 @@ public class OAuthSuccessHandler extends SavedRequestAwareAuthenticationSuccessH
     @Override
     protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
         String redirectUrl = request.getParameter("redirect");
-        return "http://localhost:3000/oauth?redirect=" + ((redirectUrl != null) ? redirectUrl : "/");
+        return successUrl + "/oauth?redirect=" + ((redirectUrl != null) ? redirectUrl : "/");
     }
 }
