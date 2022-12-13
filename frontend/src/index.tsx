eb6d341c8c9a162cbaffdae9a6b751d299bb82da
Jaewoo Ahn <DevJaewoo@gmail.com>
2022-12-13 13:46:00 +0900

    Refactor(frontend): 불필요한 SCSS 모듈 제거

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.scss";
+import "./index.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { RecoilRoot } from "recoil";
 import App from "./App";
